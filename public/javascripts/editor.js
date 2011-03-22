67e4667557e61715acaf17c549c1d620c6854f0b
Aleks Totic <a@totic.org>
2011-03-21 21:40:34 -0700

    ImageLoadingQueue rewritten

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,4 +1,4 @@
-"strict"
+"use strict"
 
 var PB = {
 	fn: {}
@@ -13,6 +13,7 @@ PB.fn.Timer = function(name) {
 $.extend(PB.fn.Timer.prototype, {
 	start: function() {
 		this.startMili = Date.now();
+		return this;
 	},
 	end: function(msg) {
 		this.endMili = Date.now();
@@ -44,47 +45,184 @@ $.extend(PB.fn.EventListener.prototype, {
 		if (i != -1)
 			this.listeners[eventType].splice(i, 1);
 	},
-	send: function(eventType, arg) {
+	send: function(eventType /* optional args */) {
 		if (!eventType in this.listeners)
 			throw "unknown event type " + eventType;
-		this.listeners[eventType].forEach(function(val) {
-			val.call(arg);
-		});
+		for (var i=0; i < this.listeners[eventType].length; i++) {
+			var f = this.listeners[eventType][i];
+			switch(arguments.length) {
+			case 1:
+				f.call(); 
+				break;
+			case 2:
+				f.call(null, arguments[1]); 
+				break;
+			case 3:
+				f.call(null, arguments[1], arguments[2]); 
+				break;
+			default:
+				throw("Cannot send this many arguments: " +(arguments.length - 1));
+			}
+		};
 	}
 });
 
+// Book class
+
+PB.fn.Book = function() {
+	this.images = [];
+	this.pages = [];
+	$.extend(this, new PB.fn.EventListener("imageAdded imageRemoved"));
+};
+
+// all events are based off book
+$.extend(PB.fn.Book.prototype, {
+	
+	addLocalFileImage: function (file) {
+		this.images.forEach(function(image) {
+			if (image.name == file.name) {
+				PB.UI.notice("Image " + file.name + " was already in the photo book.");
+				return;
+			}
+		});
+		var image = new PB.fn.BookImage(file);
+		var pos = this.images.push(image);
+		this.send('imageAdded', image, pos);
+	}
+});
+
+// Photobook image class
+PB.fn.BookImage = function(file) {
+	this.file = file;
+	this.img = null;
+}
+
+$.extend(PB.fn.BookImage.prototype, {
+
+	file: null, // on-disk file
+
+	img: null, // img tag holding the image
+	error: null,
+	
+	name: function() {
+		return this.file.name;
+	},
+	
+	toCanvasFinalize: function(deferred, options) {
+		if (this.error != null) {
+			console.log(this.name() + " failed to load");
+			return deferred.rejectWith(document, [this]);
+		}
+		console.log(this.name() + " loaded");
+		// Resize image to canvas
+		var scale = options.desiredHeight / this.img.naturalHeight;
+		if (scale > 1)
+			scale = 1;
+		var canvasWidth = Math.round(this.img.naturalWidth * scale);
+		var canvasHeight = options.desiredHeight;
+		var img = this.img;
+		var canvas = $("<canvas />")
+			.attr('width', canvasWidth)
+			.attr('height', canvasHeight)
+			.each(function(index, el) {
+				el.getContext('2d').drawImage(img, 0,0, canvasWidth, canvasHeight);
+			}).get(0);
+		// Complete callback
+		deferred.resolveWith(document, [canvas, this]);
+		// Clean up
+		this.clearImg();
+	},
+	
+	// Copies image to canvas
+	// Returns a deferred. done and fail will get (canvas, img) callbacks
+	toCanvas: function(options) {
+		var deferred = new $.Deferred();
+		$.extend({
+			desiredHeight: 128,
+		}, options);
+		
+		if (this.img && $(this.img).data('loaded'))
+			toCanvasFinalize(deferred, options);
+		else {
+			var THIS = this;
+			this.img = new Image();
+			$(this.img).bind({
+				load : function() {
+					$(THIS.img).data('loaded', true);
+					deferred.imageSize = 4 * THIS.img.width * THIS.img.height;
+					THIS.toCanvasFinalize(deferred, options);
+//					console.log("Loaded: " + THIS.name());
+				},
+				error : function(e) {
+					$(THIS.img).data('loaded', true);
+					THIS.error = "Image could not be loaded";
+					THIS.toCanvasFinalize(deferred, options);
+				},
+				abort: function(e) {
+					$(THIS.img).data('loaded', true);
+					THIS.error = "Image loading was aborted";
+					THIS.toCanvasFinalize(deferred, options);
+				}
+			});
+			PB.ImageLoadQueue.push(deferred, function() {
+//				console.log("Started: " + THIS.name());
+				THIS.img.src = window.URL.createObjectURL(THIS.file);				
+			});
+		};
+		return deferred;
+	},
+	
+	clearImg:function() {
+		if (this.img == null)
+		 	return;
+		window.URL.revokeObjectURL(this.img.src);
+		this.img.src = "";
+		this.img = null;
+	}
+
+});
+
+//
+// PB global functions
+//
+$.extend(PB, new PB.fn.EventListener("docLoaded"));
+
 $.extend(PB, {
 	init: function () {
-		this.doc = PB.fn.Document();
-		PB.UI.init();
+		this._book = new PB.fn.Book();
+	},
+
+	book: function() {
+		return this._book;
 	},
 	
 	handleFiles: function (files) {
-		for (var i=0; i<files.length; i++)
-			PB.ImageList.add(files.item(i));
+		for (var i=0; i<files.length; i++) {
+			this._book.addLocalFileImage(files.item(i));
+		}
 	},
 	
 	load: function(id) {
 		// loads the photo book document
 		alert(id);
+	},
+	stopEvent: function(e) {
+		e.stopPropagation();
+		e.preventDefault();		
 	}
 });
-$.extend(PB, new EventListener("docLoaded"));
+
+$(document).ready(function() { PB.init()});
 
 PB.UI = {};
 
 $.extend(PB.UI, {
-	
-	stopEvent: function(e) {
-		e.stopPropagation();
-		e.preventDefault();		
-	},
-	
+		
 	initSetup: $(document).ready(function() { PB.UI.init() }),
 	
 	init: function() {
 		PB.UI.initNavTabs();
-		$(document).bind('drop dragover dragenter dragleave', PB.UI.stopEvent);
+		$(document).bind('drop dragover dragenter dragleave', PB.stopEvent);
 		$(window).resize(function(e) {
 			var newHeight = $(window).height() - $("#header").outerHeight();
 			$("#main-container").css("height", newHeight + "px");
@@ -152,12 +290,12 @@ PB.UI.Phototab = {
 				'dragenter': function(e) {
 		//			console.log("dragenter" + e.currentTarget);
 					$("#photos-tab").addClass('drop-feedback');
-					PB.UI.stopEvent(e);	
+					PB.stopEvent(e);	
 				},
 				'dragleave': function(e) {
 		//			console.log("dragleave" + e.currentTarget);
 					$("#photos-tab").removeClass('drop-feedback');
-					PB.UI.stopEvent(e);			
+					PB.stopEvent(e);			
 				},
 				'drop': function(e) {
 		//			console.log("drop" + e.currentTarget);
@@ -166,12 +304,14 @@ PB.UI.Phototab = {
 				},
 				'dragover': function(e) {
 					$("#photos-tab").addClass('drop-feedback');
-					PB.UI.stopEvent(e);
+					PB.stopEvent(e);
 				}
 		};
 		// accept dragged images
 		$("#photos-tab").bind(imageTabDragEvents);
-		PB.ImageList.bind(PB.UI.Phototab);
+		PB.book().bind('imageAdded', function(image, index) {
+			PB.UI.Phototab.imageAdded(image, index);
+		});
 
 		// click to select files
 		$("#photos-tab").click(function(e) {
@@ -182,7 +322,7 @@ PB.UI.Phototab = {
 			e.stopPropagation();
 		});
 		$("#file-dialog").change(function(e) {
-			PB.UI.stopEvent(e);
+			PB.stopEvent(e);
 			PB.handleFiles(this.files);
 		});
 		// slider initialization
@@ -241,7 +381,7 @@ PB.UI.Phototab = {
 		var canvas = $("<canvas />")
 			.attr('width', allPhotos.length * hsize)
 			.attr('height', vsize).get(0);
-		c2d = canvas.getContext('2d');
+		var c2d = canvas.getContext('2d');
 		allPhotos.each(function(index, el) {
 			c2d.drawImage(el, index * hsize, 0, hsize, vsize);
 		});
@@ -249,36 +389,42 @@ PB.UI.Phototab = {
 		$("#photo-list-slider").css("width", allPhotos.length * hsize);
 	},
 
-	imageAdded: function(pbimage, index) {
-		pbimage.toCanvas({
-			desiredHeight: 128,
-			callback: function(canvas, img) {
-				$(canvas).draggable({ "containment": "body"}).appendTo('#photo-list');
-				
-				$("#photos-tab .intro").hide();
-				// Resize the container to fit the images
-				var newWidth = 0;
-				var allPhotos = $("#photo-list canvas");
-				allPhotos.each( function(index, el) {
-					var style = window.getComputedStyle(el);
-					$.each(['width', 'margin-left', 'margin-right', 'border-left-width',
-					 				'border-right-width', 'padding-left', 'padding-right',], 
-						function(i,key) {
-							newWidth += parseFloat(style.getPropertyValue(key));
-						});
-				});
-				$("#photo-list").css("width", newWidth+"px");
-				// Resize the slider
-				$("#photo-list-slider").slider("option", {
-					min: 0,
-					max: allPhotos.size() - 1,
-					value: Math.max(allPhotos.size() - 2, 0)
-				});
-				PB.UI.Phototab.restyleSlider();
-				$("#photo-list-slider").show();
-				pbimage.clearImg();
-			}
+	addNewCanvas: function(canvas, img) {
+		$(canvas).draggable({ "containment": "body"}).appendTo('#photo-list');
+		// Resize the container to fit the images
+		var newWidth = 0;
+		var allPhotos = $("#photo-list canvas");
+		allPhotos.each( function(index, el) {
+			var style = window.getComputedStyle(el);
+			newWidth += ['width', 'margin-left', 'margin-right', 
+				'border-left-width', 'border-right-width', 
+				'padding-left', 'padding-right']
+				.reduce(function(sum, prop) {
+					return sum + parseFloat(style.getPropertyValue(prop));
+				}, 0);
+			});
+		$("#photo-list").css("width", newWidth+"px");
+		// Resize the slider
+		$("#photo-list-slider").slider("option", {
+			min: 0,
+			max: allPhotos.size() - 1,
+			value: Math.max(allPhotos.size() - 2, 0)
 		});
+		PB.UI.Phototab.restyleSlider();
+		$("#photo-list-slider").show();
+		img.clearImg();
+	},
+	
+	imageAdded: function(pbimage, index) {
+		var THIS = this;
+		pbimage.toCanvas( { desiredHeight : 128 } )
+			.done( function(canvas, img) {
+				$("#photos-tab .intro").hide();
+				THIS.addNewCanvas(canvas, img);
+			})
+			.fail( function(img) {
+				alert("Could not load image " + img.name());
+			});
 	},
 	
 	imageRemoved: function(pbimage, index) {
@@ -288,166 +434,92 @@ PB.UI.Phototab = {
 
 $(document).ready(PB.UI.Phototab.init);
 
-PB.LoadImageQueue = {
+// Image loading queue
+// Limits how many images:
+// - can be downloaded simultaneusly
+// - can be downloaded in a 10 second window. This is to prevent
+//   memory trashing, FF keeps all images in used memory for 10 seconds, 
+//   unused for 25. When loading images off local disk, single image can be
+//   4928 x 3264 x 4 = 60MB undecoded.
+// 	 TestPix loads 100 images in 64s
+// gfx/surface/image cache can still grow to 1.X
 
-	queue: [],
-	active: 0,
-	timer: null,
-	timedTotal: 0,
+PB.ImageLoadQueue = {
+	activeLimit: 20,
+	liveLimit: 1,	// concurrent image loads
+	timeLimit: 10, // how long do we count a job to hold image in memory
+	imageCacheLimit: 600 * 1048576,	// 600MB
+	active: [],	// Array of [deferred, timeExpires, live, size]
+	waiting: [], // Array of [deferred, fn]
 	
-	push: function(fn) {
-		if (this.timer == null) {
-			this.timer = new Timer("Image loader " + Date.now());
-			this.timedTotal = 0;
-		}
-		this.queue.push(fn);
-		window.setTimeout(function() {
-			// Delay execution because ui interactions during drop event causes bugs
-			PB.LoadImageQueue.process();			
-		});
+	activeObject: function(deferred) {
+		this.deferred = deferred;
+		this.size = 0;
+		this.expireTime = Date.now() + PB.ImageLoadQueue.timeLimit * 1000;
+		this.live = true;
+		this.imageSize = 0;
 	},
-
-	functionDone: function() {
-		this.active -= 1;
+	push: function(deferred, fn) {
+		this.waiting.push([deferred, fn]);
 		this.process();
 	},
-	
-	process: function() {
-		if (this.queue.length == 0) {
-			this.timer.end(" " + this.timedTotal + " image loaded in ");
-			this.timer = null;
-		}
-		if (this.active > 0 || this.queue.length == 0)
-			return;
-		var fn = this.queue.shift();
-		this.timedTotal += 1;
-		this.active += 1;
-		// Rate-limit how fast the queue can fire
-		// 40MB image loads in 1/3s
-		// Firefox clears its cache after 10s, our mem usage goes up to 1.5G, causing trashing
-		// TODO, measure memory usage in the last 10 seconds, and rate-limit on 400MB
-		window.setTimeout(fn, 400);
-	}
-
-};
-
-var PB.fn.Book = function() {
-	this.images = [];
-	this.pages = [];
-	$.extend(this, new EventListener("imageAdded imageRemoved"));
-};
-
-// all events are based off book
-$.extend(PB.fn.Book.prototype, {
-	title: "",
-});
-
-// Photobook image class
-PB.fn.Image = function(file) {
-	this.file = file;
-	this.img = null;
-}
-
-$.extend(PB.fn.Image.prototype, {
-
-	file: null, // on-disk file
-
-	img: null, // img tag holding the image
-	
-	name: function() {
-		return this.file.name;
-	},
-	
-	toCanvasFinalize: function(options) {
-		console.log(this.name() + " loaded");
-		// Resize image to canvas
-		var scale = options.desiredHeight / this.img.naturalHeight;
-		if (scale > 1)
-			scale = 1;
-		var canvasWidth = Math.round(this.img.naturalWidth * scale);
-		var canvasHeight = options.desiredHeight;
-		var img = this.img;
-		var canvas = $("<canvas />")
-			.attr('width', canvasWidth)
-			.attr('height', canvasHeight)
-			.each(function(index, el) {
-				el.getContext('2d').drawImage(img, 0,0, canvasWidth, canvasHeight);
-			}).get(0);
-		options.callback(canvas, this);
-		// Clean up
-		this.clearImg();
-	},
-	
-	toCanvas: function(options) {
-		var defaults = {
-			desiredHeight: 128,
-			callback: function(canvas) { console.log("No toCanvas callback") },
-			error: function(msg) { console.log("No toCanvas error callback")}
-		};
-		$.extend(defaults, options);
-		if (this.img && $(this.img).data('loaded'))
-			toCanvasFinalize(options);
-		else {
-			var pbimage = this;
-			PB.LoadImageQueue.push(function() {
-				pbimage.img = document.createElement("img");
-				pbimage.img.onload = function() {
-					try {
-						$(pbimage.img).data('loaded', true);
-						pbimage.toCanvasFinalize(options);
-					}
-					finally {
-						PB.LoadImageQueue.functionDone();
-					}
-				}
-				pbimage.img.src = window.URL.createObjectURL(pbimage.file);				
-			});
-		}
-	},
-	
-	clearImg:function() {
-		if (this.img == null)
-		 	return;
-		window.URL.revokeObjectURL(this.img.src);
-		this.img = null;
-	}
-
-});
-
-PB.ImageList = {
-	
-	images: [],	// Array of PB.Image
-	
-	listeners: [],
-	
-	add: function(file) {
-		// Do not import duplicate file names
-		this.images.forEach(function(image) {
-			if (image.name == file.name) {
-				PB.UI.notice("Image " + file.name + " was already in the photo book.");
-				return;
+	execute: function(deferred, fn) {
+		this.active.push(new this.activeObject(deferred));
+		var THIS = this;
+		deferred.done(function() {
+			// Mark job as inactive
+			for (var i=0; i< THIS.active.length; i++)
+				if (THIS.active[i].deferred == deferred) {
+					THIS.active[i].live = false;
+					if ('imageSize' in deferred)
+						THIS.active[i].imageSize = deferred.imageSize;
 			}
+			THIS.timer.imagesLoaded += 1;
+			THIS.process(); // Process any new jobs
 		});
-
-		var image = new PB.fn.Image(file);
-		var pos = this.images.push(image);
-		
-		this.listeners.forEach(function(listener) {	// broadcast addition
-			listener.imageAdded(image, pos);
-		});
+		fn();
 	},
-
-	remove: function() {	// TODOs
-	},
-
-	bind: function(listener) {
-		this.listeners.push(listener);
-		return listener;
-	},
-
-	unbind: function(handler) {
-		var i = this.listeners.indexof(handler);
-		if (i != -1)
-			this.listeners.splice(i, 1);
+	process: function() {
+		if (! ("timer" in this )) {
+			this.timer = new PB.fn.Timer("Image queue").start();
+			this.timer.imagesLoaded = 0;
+		}
+		// remove inactive jobs
+		var expiry = Date.now();
+		var liveJobs = 0;
+		var imageCacheSize = 0;
+		for (var i=0; i< this.active.length; i++) {
+			// Remove inactive/expired jobs
+			if (this.active[i].expireTime < expiry) { 
+				this.active.splice(i, 1);
+				i -= 1;
+			}
+			else {
+				imageCacheSize += this.active[i].imageSize;
+				// Count live jobs
+				if (this.active[i].live)
+					liveJobs += 1;
+			}
+		}
+		// Fill up the work queue
+		console.log("Waiting:" + this.waiting.length + " Active:" + this.active.length + " imageCacheSize:" + imageCacheSize + " liveJobs:" + liveJobs);
+		while (this.waiting.length > 0
+				&& this.active.length < this.activeLimit
+				&& imageCacheSize < this.imageCacheLimit
+				&& liveJobs < this.liveLimit) {
+			var rec = this.waiting.shift();
+			this.execute(rec[0], rec[1]);
+			liveJobs += 1;
+		}
+		// Set up heartbeat if no jobs complete and call us back
+		if (this.waiting.length > 0 && liveJobs == 0) {
+			var THIS = this;
+			window.setTimeout(function() {
+				THIS.process();
+			}, 1000);
+		}
+		this.timer.end("LIQ::process, images " + this.timer.imagesLoaded + " executed in ");
 	}
 };
+
+
