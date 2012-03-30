95bcfedf52ed467b1d3133d0b185b2c58649ffd0
Aleks Totic <a@totic.org>
2012-03-29 23:07:01 -0700

    deploy:init_script rake task

--- script/common.rb
+++ script/common.rb
@@ -10,10 +10,10 @@ end
 
 def get_cmd_line(options)
 	cmd_line = [ 
-		"bin/thin",
+		"thin",
 		ARGV[0]
 	]
-	[:port, :rackup, :pid,:log,:tag, :timeout].each do |o|
+	[:port, :rackup, :pid,:log,:tag, :timeout, :environment].each do |o|
 		cmd_line.push("--#{o.to_s} #{options[o]}") if options.has_key? o
 	end
 	[:daemonize, :debug, :force].each do |o|
@@ -32,6 +32,8 @@ def process(options)
 		options[:daemonize] = true
 	end
 
+	options = options.merge({ :environment => SvegSettings.environment.to_s})
+
 	usage
 
 	if (ARGV[0].eql? 'dump_conf')
@@ -41,11 +43,53 @@ def process(options)
 	else
 		cmd_line = get_cmd_line(options)
 		if options[:debug]
-			Kernel.exec cmd_line
+			Kernel.exec get_cmd_line(options)
 		else
 			out = `#{cmd_line} 2>&1`
 			puts out unless out.empty?
 			puts "Error " unless $?.success?
 		end
 	end
+end
+
+def generate_init_script(server_name)
+	thin_bin = `which thin`.chomp
+	script = <<-eos
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          #{server_name}
+# Required-Start:    $local_fs $remote_fs $network
+# Required-Stop:     $local_fs $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      S 0 1 6
+# Short-Description: ${server_name} initscript
+# Description:       #{server_name} part of the pb4us server family
+### END INIT INFO
+
+# Do NOT "set -e"
+
+DAEMON=#{thin_bin}
+SCRIPT_NAME=/etc/init.d/#{server_name}
+CONFIG_PATH=/etc/thin/
+
+# Exit if the package is not installed
+[ -x "$DAEMON" ] || exit 0
+
+case "$1" in
+  start)
+	$DAEMON start --all $CONFIG_PATH
+	;;
+  stop)
+	$DAEMON stop --all $CONFIG_PATH
+	;;
+  restart)
+	$DAEMON restart --all  $CONFIG_PATH
+	;;
+  *)
+	echo "Usage: $SCRIPT_NAME {start|stop|restart}" >&2
+	exit 3
+	;;
+esac
+eos
+	script
 end
