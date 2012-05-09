e70d7f898ad4cf44a5dce523f99b2864bd25f7d0
Aleks Totic <a@totic.org>
2012-05-09 15:07:22 -0700

    JsonDiff/JsonPath implementation with tests. Works

--- public/js/diff.js
+++ public/js/diff.js
@@ -13,7 +13,7 @@ patch format:
 http://c2.com/cgi/wiki?DiffAlgorithm
 */
 
-/** 
+/************************************************************************************
 	JsonPath implementation
 
 	JsonPath(obj, path) => [ encapsulated_results* ]
@@ -38,7 +38,7 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 		match[i].path(); // returns path
 		console.log(match[i]); // print path/value
   }
-*/
+***************************************************************************************/
 
 "use strict";
 (function(window) {
@@ -207,8 +207,89 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 
 })(this);
 
+/*
+// DoublyLinkedList
+// simple homemade implementation
+(function(window) {
+	function DoublyLinkedListItem(val) {
+		this.prev = null;
+		this.next = null;
+		this.val = val;
+	}
+	function DoublyLinkedList() {
+		this.first = null;
+		this.last = null;
+	}
+	DoublyLinkedList.fromArray = function(arry) {
+		var list = new DoublyLinkedList();
+		for (var i =0; i< arry.length; i++)
+			list.pushVal(arry[i]);
+		return list;
+	}
+	DoublyLinkedList.prototype = {
+		// Returns array of DoublyLinkedListItems, if you want that kind of thing
+		toItemArray: function() {
+			var array = [];
+			this.each(function(item) {
+				array.push(item);
+			});
+			return array;
+		},
+		// Returns item values as array. If asItems true, returns array of DoublyLinkedListItem
+		toArray: function(asItems) {
+			var array = [];
+			this.each( function(item) {
+				array.push(asItems ? item : item.val);
+			});
+			return array;
+		},
+		each: function(cb) {
+			var item = this.first;
+			while (item) {
+				cb(item);
+				item = item.next;
+			}
+		},
+
+		pushVal: function(val) {
+			var item = new DoublyLinkedListItem(val);
+			if (this.first == null) {
+				this.first = this.last = item;
+			}
+			else {
+				this.last.next = item;
+				item.prev = this.last;
+				this.last = item;				
+			}
+			return item;
+		},
+		remove: function(item) {
+			if (item == this.first)
+				this.first = item.next;
+			else if (item == this.last)
+				this.last = item.prev;
+			if (item.prev)
+				item.prev.next = item.next;
+			if (item.next)
+				item.next.prev = item.prev;
+		}
+	}
+
+	window.DoublyLinkedList = DoublyLinkedList;
+})(this);
+*/
+
 // JsonDiff
 (function(window) {
+
+	function mergeOptions(options, defaults)
+	{
+		options = options || {};
+		for (var p in defaults)
+			if (!options.hasOwnProperty(p))
+				options[p] = defaults[p];
+		return options;
+	}
 
 	function getType(obj) {
 		if (obj instanceof Array)
@@ -219,63 +300,111 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 			return 'Basic';
 	}
 
+	function iterateObject(obj, callback) {
+		switch(getType(obj)) {
+			case 'Array':
+				for (var i=0; i<obj.length; i++)
+					callback(obj, i);
+		}
+	}
 	function createUpdate(path, value, oldValue) {
-		var update =  {
-			'type': 'update',
-			'path': path,
-			'args': value
-		}
-		var implicit_delete = {
-			'type': 'implicit_delete',
-			'path': path,
-			'args': oldValue
-		}
-		return [update, implicit_delete];
+/*		var retVal = createDelete(path, oldValue, {'implicit': true}); 
+		var retVal = retVal.concat( createInsert(path, value));*/
+		return createInsert(path, value);
+//		return retVal;
+	}
+
+	function createSimpleInsert(path, value) {
+		return { 'op' : 'insert', 'path': path, 'args' : value }
 	}
 
 	function createInsert(path, value) {
-		return [{
-			'type': 'insert',
-			'path': path,
-			'args': value
-		}]
+		var retVal = [];
+		switch(getType(value)) {
+			case 'Object': // Insert empty object, populate props
+				retVal.push( createSimpleInsert(path, {}));
+				for (var p in value)
+					retVal = retVal.concat( createInsert(path + "." + p, value[p]));
+				break;
+			case 'Array': // Insert empty array, populate props
+				retVal.push( createSimpleInsert(path, []));
+				for (var i = 0; i<value.length; i++)
+					retVal = retVal.concat(createInsert(path + "." + i, value[i]));
+				break;
+			case 'Basic':
+				retVal.push( createSimpleInsert(path, value));
+				break;
+		}
+		return retVal;
 	}
 
-	function createDelete(path, value) {
-		return [ {
-			'type': 'delete',
-			'path': path,
-			'args': value
-		}]
+	function createSimpleDelete(path, value, options) {
+		return { 'op': 'delete', 'path': path, 'args': value, 'implicit': options['implicit'] }
 	}
 
+	// options: implicit: true|false
+	// implicit means that the delete can be discarded without changing patch results
+	//    implicit is used to record values 'implicitly' deleted when field is updated
+	//    createUpdate function uses this.
+	function createDelete(path, value, options) {
+		options = mergeOptions(options, {implicit: false});
+
+		var retVal = [];
+
+		switch (getType(value)) {
+			case 'Object':
+				for (var p in value)
+					retVal = retVal.concat( createDelete(path + "." + p, value[p], options));
+				retVal.push( createSimpleDelete(path, value, options));
+				break;
+			case 'Array':
+				for (var i=value.length-1; i>=0; i--)
+					retVal = retVal.concat( createDelete(path + "." + i, value[i], options));
+				retVal.push( createSimpleDelete(path, value, options));
+				break;
+			case 'Basic':
+				retVal.push( createSimpleDelete(path, value, options));
+				break;
+		}
+		return retVal;
+	}
+/*
+	function createMove(src, dest) {
+		return { op: 'move', path: src, args: dest}
+	}
+*/
 	function applyDiff(obj, diff) {
-		switch(diff.type) {
-			case 'update':
-				var target = JsonPath(obj, diff.path, {'just_one':true });
-				if (target)
-					target.set(diff.args);
-				else
-					throw "Could not UPDATE, target not found";
-				break;
-			case 'implicit_delete':
-				break;
+		switch(diff.op) {
 			case 'insert':
 				var target = JsonPath(obj, diff.path, {'just_one': true, 'ghost_props': true});
 				if (target)
 					target.set(diff.args);
 				else
-					throw "Cound not INSERT, target not found";
+					throw "Could not INSERT, target not found";
 				break;
 			case 'delete':
+				if (diff.implicit)
+					break;
 				var target = JsonPath(obj, diff.path, {'just_one':true });
 				if (target)
 					target.delete();
 				else
 					throw "Could not DELETE, target not found";
 				break;
+/*			case 'move':
+				var src = JsonPath(obj, diff.path, {'just_one':true});
+				var dest = JsonPath(obj, diff.args, {'just_one': true, 'ghost_props':true});
+				if (src && dest) {
+					dest.set(src.val());
+					src.delete();
+				}
+				else
+					throw "Could not MOVE" 
+						+ (src ? "" : diff.path + " not found ") 
+						+ (dest ? "" : diff.args + " not found ");
+				break; */
 			default:
-				throw "Unknown operation " + diff.type;
+				throw "Unknown operation " + diff.op;
 		}
 	}
 
@@ -283,7 +412,10 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 		if (diff instanceof Array)
 			diff.forEach( function(d) { printDiff(d)});
 		else {
-			console.log(diff.path, " ", diff.type, " ", diff.args);
+			if (diff.implicit)
+				console.warn(diff.path, " ", diff.op, " ", diff.args);
+			else
+				console.log(diff.path, " ", diff.op, " ", diff.args);
 		}
 		return "";
 	}
@@ -306,7 +438,7 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 		var diff = [];
 		justOld.forEach( function(p) { 
 			diff = diff.concat(
-				createDelete(path + "." + p));
+				createDelete(path + "." + p, oldObj[p]));
 		});
 		justNew.forEach( function(p) {
 			diff = diff.concat(
@@ -366,22 +498,165 @@ http://c2.com/cgi/wiki?DiffAlgorithm
 
 	// Removes crud: implicit ops, delete args
 	function cleanupDiff(diff) {
-		// TODO
-	}
-	function compressDiff(oldObj, newObj, diff)
-	{
-		return diff;
+		var newDiff = [];
+		for (var i=0; i<diff.length; i++) {
+			if (!('implicit' in diff[i]) || (diff[i].implicit == false)) {
+				if ('implicit' in diff[i])
+					delete diff[i].implicit;
+				switch(diff[i].op) {
+					case 'delete':
+						diff[i].args = null;
+						break;
+					default:
+						break;
+				}
+				newDiff.push(diff[i]);
+			}
+		}
+		return newDiff;
 	}
 
-	function jsonDiff(oldObj, newObj) {
+	function compareDiffArgs(a,b) {
+		var type_a = getType(a.args);
+		var type_b = getType(b.args);
+		if (type_a == type_b) 
+			switch(type_a) {
+				case 'Object':
+				case 'Array':
+					return 0;
+				default:
+					return a.args == b.args ? 0 : a.args < b.args ? -1 : 1;	// a <=> b
+			}
+		else  // Object > Array > Basic
+			if (type_a == 'Basic')
+				return -1;
+			else if (type_b == 'Basic')
+				return 1;
+			else if (type_a == 'Array')
+				return -1;
+			else 
+				return 1;
+	}
+
+	function compareDiffPaths(a,b) {
+		if (a.path < b.path)
+			return -1;
+		else if (b.path < a.path)
+			return 1;
+		else 
+			return 0;
+	}
+
+	/* Optimizations did not work
+	I was hoping to replace delete/insert pairs with a single move
+	This fails because delete and insert position in diff queue is important:
+	- insert: parents must be inserted before the child
+	- delete: children must be deleted before the parent
+	The main motivation for move was to avoid massive diffs when an array is shuffled
+	This would happen if pages were kept as an array
+	Takeaway: minimize array use.
+
+	function optimizeDiff(oldObj, newObj, diff)
+	{
+		function optimizeToMove(diffList, insertOp, deleteOp)
+		{
+			var src = deleteOp.val.path;
+			// if src is an array indice path with index X
+
+			// Converting deleteOp to move on an array item will make later indices be off by 1
+			// We must find and patch all of them
+			var isArrayMatch = src.match(/^(.*\.)(\d+)$/);	// "a.b.c.1" means c is an array
+
+			if (isArrayMatch) {
+				// shift all indices into this array in diffs after deleteOp diff
+				var srcIndex = parseInt(isArrayMatch[2]);
+				// match paths that reference the same array
+				// create a matching regex, escape special characted
+				// "a.b.c.1" would create a regex: /(a\.b\.c\.)(\d+)
+				var matchingRegex = new RegExp( 
+					"^(" + isArrayMatch[1].replace(/[\\\^\$\*\+\?\(\)\[\]\.\!\:\|\{\}\/]/g, "\\$&") + ")"
+					+ "(\\d+)" + "(.*)$" 
+				);
+				var next = insertOp.next;
+				while (next) {
+					var isPathMatch = matchingRegex.exec(next.val.path);
+					if (isPathMatch) {
+						var matchingIndex = parseInt(isPathMatch[2]);
+						if (matchingIndex > srcIndex) {
+							next.val.path = isPathMatch[1] + (matchingIndex - 1) + isPathMatch[3];
+						}
+					}
+					next = next.next;
+				}
+			}
+			// change delete to move, and remove the insertOp
+			var moveDiff = createMove(deleteOp.val.path, insertOp.val.path);
+			deleteOp.val = moveDiff;
+			diffList.remove(insertOp);
+		}
+
+		// Create doubly-linked list of diffs. Used for easy insert/remove
+		var diffList = DoublyLinkedList.fromArray(diff);
+		// Sort diffs by arg values
+		var diffItemArray = diffList.toArray(true);
+		diffItemArray.sort(function(a,b) {
+			return compareDiffArgs(a.val, b.val);
+		});
+		// Separate arg values into buckets
+		var buckets = [];
+		var currBucket = null;
+		for (var i=0; i<diffItemArray.length; i++)
+		{
+			if (currBucket != null && compareDiffArgs(currBucket[0].val, diffItemArray[i].val) == 0)
+				currBucket.push(diffItemArray[i]);
+			else {
+				currBucket = [diffItemArray[i]];
+				buckets.push(currBucket);
+			}
+		}
+		// filter out the non-moving buckets
+		buckets = buckets.filter(function(x) { return x.length > 1});
+		// replace matching delete/inserts with moves
+		for (var i=0; i<buckets.length; i++)
+		{
+			// cannot move arrays or objects yet
+			if (buckets[i].length < 2 || getType(buckets[i][0].val.args) != 'Basic')
+				continue;
+			var inserts = [];
+			var deletes = [];
+			// group values into inserts and deletes
+			for (var j=0; j < buckets[i].length ; j++)
+				switch(buckets[i][j].val.op) {
+					case 'insert':
+						inserts.push(buckets[i][j]); break;
+					case 'delete':
+						deletes.push(buckets[i][j]); break;
+					default:
+						break;
+				}
+			// create the moves
+			var possibleMoves = Math.min(inserts.length, deletes.length);
+			for (var j=0; j < possibleMoves; j++)
+				optimizeToMove(diffList, inserts[j], deletes[j]);
+		}
+		return diffList.toArray();
+	}
+	*/
+	function jsonDiff(oldObj, newObj, options) {
+		options = mergeOptions(options, {optimize: false, cleanup: false});
 		var diff = jsonDiffHelper(oldObj, newObj, "$");
-		diff = compressDiff(oldObj, newObj, diff);
+/*		if (options.optimize)
+			diff = optimizeDiff(oldObj, newObj, diff);
+*/
+		if (options.cleanup)
+			diff = cleanupDiff(diff)
 		return diff;
 	}
 
 	// Clones the object, applies diff
 	function jsonPatch(obj, diff) {
 		var newObj = JSON.parse(JSON.stringify(obj));
+		var diff = JSON.parse(JSON.stringify(diff)); // clone the diff because we do not want to modify original diff
 		for (var i =0; i< diff.length ; i++)
 		{
 			try {
